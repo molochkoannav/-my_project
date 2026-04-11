b5d71ccf8f0cb1606e2a0f53836621115b7871f5
Ann Molochko <molochkoannav@gmail.com>
2026-04-11 13:35:12 +0300

    new func get_date

--- src/wiget.py
+++ src/wiget.py
@@ -10,11 +10,20 @@ def mask_account_card (number: str) -> str:
 
     return f'{number[:-17]} {card_number}'
 
+
+def get_date(data: str) -> str:
+    data_time = data[:10].split("-")
+
+    return f'{data_time[2]}.{data_time[1]}.{data_time[0]}'
+
+
 if __name__ == '__main__':
-    print(mask_account_card("Maestro 1596837868705199"))
-    print(mask_account_card("Счет 64686473678894779589"))
-    print(mask_account_card("MasterCard 7158300734726758"))
-    print(mask_account_card("Visa Classic 6831982476737658"))
-    print(mask_account_card("Visa Platinum 8990922113665229"))
-    print(mask_account_card("Visa Gold 5999414228426353"))
-    print(mask_account_card("Счет 73654108430135874305"))
+    # print(mask_account_card("Maestro 1596837868705199"))
+    # print(mask_account_card("Счет 64686473678894779589"))
+    # print(mask_account_card("MasterCard 7158300734726758"))
+    # print(mask_account_card("Visa Classic 6831982476737658"))
+    # print(mask_account_card("Visa Platinum 8990922113665229"))
+    # print(mask_account_card("Visa Gold 5999414228426353"))
+    # print(mask_account_card("Счет 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
+
