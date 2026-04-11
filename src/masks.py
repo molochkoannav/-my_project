1cd452b52880c0364b1e1dce2c543b90d8048f94
Ann Molochko <molochkoannav@gmail.com>
2026-04-11 12:31:56 +0300

    начало

---
+++ src/masks.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+
+def get_mask_card_number(cart_number: Union[int]) -> Union[str]:
+    """Функция маскирует номер карты пользователя"""
+    list_cart_number = [number for number in (str(cart_number))]
+    list_cart_number[6:-4] = ["*", "*", "*", "*", "*", "*"]
+    for num in range(len(list_cart_number) // 4 - 1, 0, -1):
+        list_cart_number[num * 4 : num * 4] = [" "]
+
+    mask_cart_number = "".join(list_cart_number)
+    return mask_cart_number
+
+
+def get_mask_account(account_number: Union[int]) -> Union[str]:
+    """Функция маскирует номер счета пользователя"""
+    list_account_number = [number for number in (str(account_number))]
+    list_account_number[:-4] = ["*", "*"]
+    mask_account = "".join(list_account_number)
+
+    return mask_account
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(73654108430135874305))
