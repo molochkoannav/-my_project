a9289a356bad4c6e2cc8e1027993192332c92324
Ann Molochko <molochkoannav@gmail.com>
2026-04-19 09:47:39 +0300

    fix bugs in module 'processing'

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
 def filter_by_state(api_list: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """Функция фильтрует списки словарей, по ключу 'state'"""
-    filtred_api_list = []
+    """Функция фильтрует списки словарей по ключу 'state'"""
+    filtered_api_list: list[dict] = []
 
     for api in api_list:
         if api.get("state") == state:
-            filtred_api_list.append(api)
+            filtered_api_list.append(api)
 
-    return filtred_api_list
+    return filtered_api_list
 
 
 def sort_by_date(api_list: list[dict], sort_by: str = "date", reverse: bool = True) -> list[dict]:
