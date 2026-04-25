ce6bc643d583df7aca7f24b2fc03dfe3e6bb9363
Ann Molochko <molochkoannav@gmail.com>
2026-04-25 13:15:14 +0300

    test_masks done

--- src/masks.py
+++ src/masks.py
@@ -2,23 +2,26 @@ from string import digits
 from typing import Union
 
 
-def get_mask_card_number(cart_number: Union[str]) -> Union[str]:
+def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция маскирует номер карты пользователя"""
-    if len(cart_number) == 16 and cart_number.isdigit():
-            list_cart_number = [number for number in (str(cart_number))]
-            list_cart_number[6:-4] = ["*", "*", "*", "*", "*", "*"]
-            for num in range(len(list_cart_number) // 4 - 1, 0, -1):
-                list_cart_number[num * 4: num * 4] = [" "]
+    if len(card_number) == 16 and card_number.isdigit():
+            list_card_number = [number for number in (str(card_number))]
+            list_card_number[6:-4] = ["*", "*", "*", "*", "*", "*"]
+            for num in range(len(list_card_number) // 4 - 1, 0, -1):
+                list_card_number[num * 4: num * 4] = [" "]
 
-            mask_cart_number = "".join(list_cart_number)
-            return mask_cart_number
+            mask_card_number = "".join(list_card_number)
+            return mask_card_number
 
     else:
         return "Номер карты набран не верно"
 def get_mask_account(account_number: Union[str]) -> Union[str]:
     """Функция маскирует номер счета пользователя"""
-    list_account_number = [number for number in (str(account_number))]
-    list_account_number[:-4] = ["*", "*"]
-    mask_account = "".join(list_account_number)
+    if len(account_number) == 20 and account_number.isdigit():
+        list_account_number = [number for number in (str(account_number))]
+        list_account_number[:-4] = ["*", "*"]
+        mask_account = "".join(list_account_number)
 
-    return mask_account
+        return mask_account
+    else:
+        return "Номер счета набран не верно"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,6 @@
 import pytest
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
 @pytest.mark.parametrize("card_number, expected", [
     ('123412341234123', 'Номер карты набран не верно'),
@@ -11,3 +12,14 @@ from src.masks import get_mask_card_number
 ])
 def test_get_mask_card_number(card_number, expected):
     assert get_mask_card_number(card_number) == expected
+
+@pytest.mark.parametrize("account_number, expected", [
+    ('736541084301358743051', 'Номер счета набран не верно'),
+    ('7365410843013587430', 'Номер счета набран не верно'),
+    ('736as1084301358743051', 'Номер счета набран не верно'),
+    ('7365410 43013587 3051', 'Номер счета набран не верно'),
+    ('73654108430135874305', '**4305'),
+    ('', 'Номер счета набран не верно')
+])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
