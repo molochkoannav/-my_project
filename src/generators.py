d44bbdf58df9213fc57e6ad98970d1db6ff1b1b4
Ann Molochko <molochkoannav@gmail.com>
2026-05-01 14:18:38 +0300

    new module generators

---
+++ src/generators.py
@@ -0,0 +1,9 @@
+def filter_by_currency(transactions_list: list[dict]) ->list[dict]:
+    pass
+
+def transaction_descriptions(description: list[dict])-> str:
+    pass
+
+def card_number_generator(card_num: int)-> int:
+    pass
+def
