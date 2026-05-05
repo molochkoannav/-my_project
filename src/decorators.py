f32ec7225f7c42388c79fc71909748752fea7842
Ann Molochko <molochkoannav@gmail.com>
2026-05-05 20:34:23 +0300

    decorator is done

--- src/decorators.py
+++ src/decorators.py
@@ -1,11 +1,53 @@
 from time import time
 import datetime
-from typing import Callable
-def log(func: Callable, file_name) -> Callable:
-    pass
-    # @wraps(func: Callable)-> Callable:
-    # def wrapper(*args: Any, **kwargs: Any) -> Any:
-    #     if func(file_name):
-    #         with open (f'{func__name__}', 'a') as file:
-    #
+from functools import wraps
+from typing import Any
+from pathlib import Path
+def log(filename: [str] = None):
+    """Декоратор для логирования выполнения функции"""
+    def my_decorator(func):
+        @wraps(func)
+        def logging_wrapper(*args: Any, **kwargs: Any) -> Any:
+            """Обертка для функции которая выполняет логирование"""
 
+            result = None
+            if filename is None:
+                try:
+                    start_time = time()
+                    result = func(*args, **kwargs)
+                    end_time = time()
+                    print(f"{func.__name__} ok")
+                    print(f"Функция {func.__name__} выполнена за {end_time - start_time} секунд")
+                    print(result)
+
+                except Exception as e:
+                    print(f"{func.__name__} error:{type(e).__name__} message: {e}")
+            else:
+                try:
+                    start_time = time()
+                    result = func(*args, **kwargs)
+                    end_time = time()
+                    project_root = Path("C:/Users/kirill/Desktop/python_learing/projects/my_proj")
+                    path_file = project_root / filename
+                    with open(path_file, "a", encoding="utf-8") as file:
+                        file.write(f"{datetime.datetime.now()} Выполняется функция {func.__name__}\n")
+                        file.write(f"Функция {func.__name__} выполнена за {end_time - start_time} секунд\n")
+                        file.write(f"Результат: {result}\n")
+                except Exception as e:
+                    with open(path_file, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error:{type(e).__name__} message: {e}")
+
+            return result
+        return logging_wrapper
+    return my_decorator
+
+
+
+
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+my_function(1, 2)
