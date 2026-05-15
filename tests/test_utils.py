ed07bc84c185a28f1b974df5e7038ffcd4b96b14
Ann Molochko <molochkoannav@gmail.com>
2026-05-15 20:28:10 +0300

    some update

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+from src.utils import get_valute_transactions
+
+def test_get_valute_transactions():
+    data =
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount":
+          "amount": "31957.58",
+          "currency":
+            "name": "руб.",
+            "code": "EUR"
+
+        ,
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to":
