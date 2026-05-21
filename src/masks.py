6ba8b504214b94e5dde6b2146a6d0ef5ef364b71
Ann Molochko <molochkoannav@gmail.com>
2026-05-21 20:40:12 +0300

    lint bug fix

--- src/masks.py
+++ src/masks.py
@@ -1,29 +1,28 @@
-
-from typing import Union
 import logging
 from pathlib import Path
-
+from typing import Union
 
 current_file = Path(__file__)
 project_root = current_file.parent.parent
-log_dir = project_root / 'logs'
+log_dir = project_root / "logs"
 log_dir.mkdir(exist_ok=True)
 
-log_file = log_dir / 'masks.log'
+log_file = log_dir / "masks.log"
 
 logging.basicConfig(
     filename=log_file,
     level=logging.DEBUG,
-    format='%(asctime)s - %(funcName)s - %(levelname)s: %(message)s',
-    filemode='w',
-    encoding='utf-8')
+    format="%(asctime)s - %(funcName)s - %(levelname)s: %(message)s",
+    filemode="w",
+    encoding="utf-8",
+)
 logger = logging.getLogger(__name__)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция маскирует номер карты пользователя"""
-    logger.info('Запуск модуля маскировки')
+    logger.info("Запуск модуля маскировки")
     if len(card_number) == 16 and card_number.isdigit() and not card_number.startswith(("0", "00", "000", "0000")):
         list_card_number = [number for number in (str(card_number))]
         list_card_number[6:-4] = ["*", "*", "*", "*", "*", "*"]
@@ -31,18 +30,17 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
             list_card_number[num * 4: num * 4] = [" "]
 
         mask_card_number = "".join(list_card_number)
-        logger.info('Маскировка номера карты выполнена')
+        logger.info("Маскировка номера карты выполнена")
         return mask_card_number
 
     else:
-        logger.warning('Ошибка в наборе номера карты')
+        logger.error("Ошибка в наборе номера карты")
         return "Номер карты набран не верно"
-
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
     """Функция маскирует номер счета пользователя"""
-    logger.info('Запуск модуля маскировки счета')
+    logger.info("Запуск модуля маскировки счета")
     if (
         len(account_number) == 20
         and account_number.isdigit()
@@ -51,8 +49,8 @@ def get_mask_account(account_number: Union[str]) -> Union[str]:
         list_account_number = [number for number in (str(account_number))]
         list_account_number[:-4] = ["*", "*"]
         mask_account = "".join(list_account_number)
-        logger.info('Маскировка номера счета выполнена')
+        logger.info("Маскировка номера счета выполнена")
         return mask_account
     else:
-        logger.warning('Ошибка в наборе номера счета')
+        logger.error("Ошибка в наборе номера счета")
         return "Номер счета набран не верно"
--- src/utils.py
+++ src/utils.py
@@ -2,24 +2,23 @@ import json
 import logging
 from pathlib import Path
 
-
 current_file = Path(__file__)
 project_root = current_file.parent.parent
-log_dir = project_root / 'logs'
+log_dir = project_root / "logs"
 log_dir.mkdir(exist_ok=True)
 
-log_file_ut = log_dir / 'utils.log'
+log_file_ut = log_dir / "utils.log"
 
 logging.getLogger("urllib3").setLevel(logging.WARNING)
-logger_ut = logging.getLogger('utils')
+logger_ut = logging.getLogger("utils")
 logger_ut.setLevel(logging.DEBUG)
 
 
-file_handler_ut = logging.FileHandler(log_file_ut, mode='w', encoding='utf-8')
+file_handler_ut = logging.FileHandler(log_file_ut, mode="w", encoding="utf-8")
 file_handler_ut.setLevel(logging.DEBUG)
 
 
-formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s: %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(funcName)s - %(levelname)s: %(message)s")
 file_handler_ut.setFormatter(formatter)
 
 
@@ -32,22 +31,22 @@ def get_read_transactions(file_path):
     Принимает путь до JSON-файла
     и возвращает список словарей с
     данными о финансовых транзакциях"""
-    logger_ut.info('Запуск модуля чтения данных')
+    logger_ut.info("Запуск модуля чтения данных")
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if isinstance(data, list):
-                logger_ut.info('Данные считаны')
+                logger_ut.info("Данные считаны")
                 return data
             else:
-                logger_ut.info('Данные не считаны')
+                logger_ut.info("Данные не считаны")
                 return []
     except FileNotFoundError:
-        logger_ut.warning('Файл не найден')
+        logger_ut.warning("Файл не найден")
         return []
     except json.decoder.JSONDecodeError:
-        logger_ut.warning('Ошибка чтения данных')
+        logger_ut.warning("Ошибка чтения данных")
         return []
     except ValueError:
-        logger_ut.warning('Ошибка чтения данных')
+        logger_ut.warning("Ошибка чтения данных")
         return []
